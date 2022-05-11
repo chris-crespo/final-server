837689985c46ba7354b5e181f1f28dd0f6529655
krs98 <christian.crespoo.98@gmail.com>
2022-05-11 11:46:28 +0000

    add   routes

--- server.rkt
+++ server.rkt
@@ -1,14 +1,15 @@
 #lang racket
 
 (require web-server/http
+         web-server/http/bindings
+         web-server/http/json
          web-server/dispatch
          (prefix-in sequencer: web-server/dispatchers/dispatch-sequencer)
          web-server/servlet-dispatch
-         web-server/web-server
-         web-server/http/json
-         json)
+         web-server/web-server)
 
-(require db)
+(require db json)
+(require file/sha1) ; Used to converted the result of sha256-bytes back into a string
 
 (define (parse-url url)
   (define (skip-postgres url) (substring url 11))
@@ -34,10 +35,6 @@
                       #:ssl 'yes
                       #:password (hash-ref params 'password)))
 
-;; Negates a predicate
-(define ((neg pred) . args)
-  (not (apply pred arg)))
-
 (define ((available? column) value)
   (define query (format "select count(*) from app_user where ~a = $1" column))
   (zero? (query-value pgc query value)))
@@ -45,26 +42,98 @@
 (define username-available? (available? "username"))
 (define email-available? (available? "email"))
 
-(define username-exists? (not username-available?))
-(define email-exists? (not email-exists?))
+(define username-exists? (negate username-available?))
+(define email-exists? (negate email-available?))
+
+;;; Macros to handle request params and post data
+(define-syntax-rule (with-request-params request (param ...) expr0 . exprs)
+  (begin
+    (define bindings (request-bindings/raw request))
+    (let ((param (bind-id (symbol->bytes (quote param)) bindings)) ...) 
+      expr0 . exprs)))
+
+(define-syntax-rule (with-post-data request (param ...) expr0 . exprs)
+  (begin
+    (define data (bytes->jsexpr (request-post-data/raw request)))
+    (let ((param (hash-ref data (quote param) #f)) ...)
+      expr0 . exprs)))
+
+;;; Request parameter binding utilities
+(define symbol->bytes (compose string->bytes/utf-8 symbol->string))
+
+;; Converts arg to sql-null if it's #f
+(define/match (->sql-null arg)
+  [(#f) sql-null]
+  [(_) arg])
+
+(define (bind-id id bindings)
+  (match
+    (bindings-assq id bindings)
+    [(? binding:form? b)
+     (bytes->string/utf-8 (binding:form-value b))]
+    [_ #f]))
+
+(define (bind-list keys bindings)
+  (define (bind key) (bind-id key bindings))
+  (map bind keys))
+
+;;; Error handlers
+(define (handlers/register e)
+  (define info (exn:fail:sql-info e))
+  (define message (cdr (assoc 'message info)))
+  (hasheq 'message message))
+
+;;; Api routes
+(define (api/auth req)
+  (with-request-params req (user password)
+    (define user-password 
+      (query-maybe-value 
+        pgc "select password from app_user where username = $1 or email = $1"
+        user))
+    (define (valid-password? user-password) 
+      ;; TODO: not passing password as parameter returns sql-null
+      (string=? password user-password))
+    (response/jsexpr
+      (if password
+          (hasheq 'user #t 'password (valid-password? user-password))
+          (hasheq 'user #f 'pasword #f)))))
 
 (define (api/available req)
-  ;; TODO: use request variables
-  (response/jsexpr
-    (hasheq 'username (username-available? "croissant")
-            'email (email-available? "croissant"))))
+  ;; The response is a json object with information about
+  ;; both the email and username availability. It always
+  ;; returns both independent of the request parameters,
+  ;; which works fine for the way the web client uses the
+  ;; api, but a better approach would be to just return
+  ;; the availability for the given request parameters.
+  (with-request-params req (username email)
+    (define username? (username-available? (->sql-null username)))
+    (define email? (email-available? (->sql-null email)))
+    (response/jsexpr
+      (hasheq 'username (and username username?)
+              'email (and email email?)))))
 
 (define (api/register req)
-  ;; TODO: register user in database using request variables (json or url?)
-  ;; TODO: should we handle errors that could arise from inserting duplicated
-  ;; data? 
-  (response/jsexpr))
-
+  (define (insert-user . args)
+    (apply
+      query pgc "insert into app_user values ($1, $2, $3, $4, $5, $6)"
+      (map ->sql-null args)))
+  (with-post-data req (username email password first-name last-name phone)
+    (response/jsexpr
+      (with-handlers ([exn:fail:sql? handlers/register])
+        (insert-user 
+          username 
+          email 
+          (bytes->hex-string (sha512-bytes (string->bytes/utf-8 password))) 
+          first-name 
+          last-name 
+          phone) 
+        (hasheq 'success #t)))))
 
 (define-values (app reverse-uri)
   (dispatch-rules
+    [("api" "auth") api/auth]
     [("api" "available") api/available]
-    [("api" "register") api/register]
+    [("api" "register") #:method "post" api/register]
     ))
 
 (define (not-found req)
